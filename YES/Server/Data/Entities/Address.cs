3d16c48945fed6609009bbca4cf6771c7a8b94e4
Kobe Delobelle <delobellekobe@gmail.com>
2021-06-10 16:29:13 +0200

    Eerste kleine seed database

--- YES/Server/Data/Entities/Address.cs
+++ YES/Server/Data/Entities/Address.cs
@@ -7,18 +7,18 @@ using YES.Server.Data.Entities;
 namespace YES.Server.Entities
 {
     public class Address : EntityBase
-    {        
+    {
         public string Street { get; set; }
         public string PostalCode { get; set; }
         public string City { get; set; }
         public int StreetNumber { get; set; }
         public string Country { get; set; }
 
-        public int VenueId { get; set; }
+        public int? VenueId { get; set; }
         public virtual Venue Venue { get; set; }
-        public int TicketProviderId { get; set; }
+        public int? TicketProviderId { get; set; }
         public virtual TicketProvider TicketProvider { get; set; }
-        public int TicketCustomerId { get; set; }
+        public int? TicketCustomerId { get; set; }
         public virtual TicketCustomer TicketCustomer { get; set; }
     }
 }
