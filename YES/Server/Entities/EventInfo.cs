a7e71b588c8d3c1ec985398fb469053f82b08dde
Impesoft <mrblubke@yahoo.com>
2021-06-10 14:27:15 +0200

    Basic Entities, and prepare for seed, Entityframework added

---
+++ YES/Server/Entities/EventInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YES.Server.Entities
+{
+    public class EventInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime EventDate { get; set; }
+
+        public int MaxAvailableTickets { get; set; }
+
+        public string WebsiteUrl { get; set; }
+
+        public string BannerImgUrl { get; set; }
+
+        public int EventId { get; set; }
+    }
+}
--- YES/Server/Entities/TicketProvider.cs
+++ YES/Server/Entities/TicketProvider.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace YES.Server.Models
+namespace YES.Server.Entities
 {
     public class TicketProvider : User
     {
-        public List<Event> Events;
+        public virtual ICollection<Event> Events { get; set; }
     }
 }
