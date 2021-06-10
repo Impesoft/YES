c97f8687b4f73501bfd1d4b12828dc0a9d9fdadd
Impesoft <mrblubke@yahoo.com>
2021-06-10 13:04:28 +0200

    some basic startup classes

---
+++ YES/Server/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YES.Server.Models
+{
+    public abstract class User
+    {
+        public int Id;
+        public string BackAccount;
+        public string Email;
+        public string PhoneNumber;
+        public Address Address;
+    }
+}
