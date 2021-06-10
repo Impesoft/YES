5e1fd537473e91b76c345439a976bc0848352254
Nick-vr <nvanroyen@gmail.com>
2021-06-10 22:36:56 +0200

    Removed material

--- YES/Server/Startup.cs
+++ YES/Server/Startup.cs
@@ -9,12 +9,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Identity.Web;
-using System.Linq;
-using Blazorise;
-using Blazorise.Bootstrap;
-using Blazorise.Icons.FontAwesome;
-using Blazorise.Icons.Material;
-using Blazorise.Material;
 
 namespace YES.Server
 {
@@ -31,13 +25,6 @@ namespace YES.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services
-                .AddBlazorise( options =>
-                {
-                    options.ChangeTextOnKeyPress = true; // optional
-                } )
-                .AddMaterialProviders()
-                .AddMaterialIcons();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddMicrosoftIdentityWebApi(Configuration.GetSection("AzureAd"));
 
@@ -76,7 +63,6 @@ namespace YES.Server
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapBlazorHub();
                 endpoints.MapRazorPages();
                 endpoints.MapControllers();
                 endpoints.MapFallbackToFile("index.html");
