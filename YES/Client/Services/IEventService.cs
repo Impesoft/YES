62cd1cc32fb156138592fccea9b59a6c36023d77
SebaK <seba.kesteloot@gmail.com>
2021-06-11 11:46:41 +0200

    added Eventservice

---
+++ YES/Client/Services/IEventService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using YES.Shared;
+
+namespace YES.Client.Services
+{
+    public interface IEventService
+    {
+        ICollection<EventDTO> GetEvents();
+        Task GetEventsFromAPIAsync();
+        void SetEventsTest();
+    }
+}
