04912843cfdb3bef308f428bf55024ef8447c059
PieterCorp <pieter@email.com>
2021-06-11 13:52:10 +0200

    EventDto mapped

---
+++ YES/Server/Business/Services/IEventService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using YES.Shared.Dto;
+
+namespace YES.Server.Business.Services
+{
+    public interface IEventService
+    {
+        Task<IEnumerable<EventDto>> GetEventsAsync();
+    }
+}
