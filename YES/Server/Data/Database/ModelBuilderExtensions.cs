b960cb549a81efb8a27e2269df18f16203939128
Kobe Delobelle <delobellekobe@gmail.com>
2021-06-11 10:55:11 +0200

    seed adressen, ticketcustomers, ticketproviders, venues, festivals

--- YES/Server/Data/Database/ModelBuilderExtensions.cs
+++ YES/Server/Data/Database/ModelBuilderExtensions.cs
@@ -45,17 +45,18 @@ namespace YES.Server.Data.Database
                 new Address { Id = 17, Street = "Tybaertstraat", StreetNumber = 27, PostalCode = "8900", City = "Ieper", Country = "België", TicketCustomerId = 15 },
                 new Address { Id = 18, Street = "Stapelhuisstraat", StreetNumber = 17, PostalCode = "8400", City = "Oostende", Country = "België", TicketCustomerId = 16 },
                 new Address { Id = 19, Street = "Graaf Van Vlaanderenplein", StreetNumber = 5, PostalCode = "9000", City = "Gent", Country = "België", VenueId = 2 },
-                new Address { Id = 20, Street = "Noordersingel", StreetNumber = 28, PostalCode = "2140", City = "Antwerpen", Country = "België", VenueId = 3 },
-                new Address { Id = 21, Street = "Overpoortstraat", StreetNumber = 60, PostalCode = "9000", City = "Gent", Country = "België", VenueId = 4 },
-                new Address { Id = 22, Street = "Schijnpoortweg", StreetNumber = 119, PostalCode = "2170", City = "Antwerpen", Country = "België", VenueId = 5 },
+                new Address { Id = 20, Street = "Noordersingel", StreetNumber = 28, PostalCode = "2140", City = "Antwerpen", Country = "België", VenueId = 3, TicketProviderId = 9 },
+                new Address { Id = 21, Street = "Overpoortstraat", StreetNumber = 60, PostalCode = "9000", City = "Gent", Country = "België", VenueId = 4, TicketProviderId = 8 },
+                new Address { Id = 22, Street = "Schijnpoortweg", StreetNumber = 119, PostalCode = "2170", City = "Antwerpen", Country = "België", VenueId = 5, TicketProviderId = 7 },
                 new Address { Id = 23, Street = "Schommelei", StreetNumber = 1, PostalCode = "2850", City = "Boom", Country = "België", VenueId = 6 },
                 new Address { Id = 24, Street = "Festivalpark", PostalCode = "3118", City = "Werchter", Country = "België", VenueId = 7 },
-                new Address { Id = 25, Street = "Eeuwfeestlaan", StreetNumber = 617, PostalCode = "1020", City = "Brussel", Country = "België", VenueId = 8 },
+                new Address { Id = 25, Street = "Eeuwfeestlaan", StreetNumber = 617, PostalCode = "1020", City = "Brussel", Country = "België", VenueId = 8, TicketProviderId = 4 },
                 new Address { Id = 26, Street = "Kempische Steenweg", PostalCode = "3500", City = "Hasselt", Country = "België", VenueId = 9 },
                 new Address { Id = 27, Street = "Binnenvaartstraat", PostalCode = "3530", City = "Houthalen-Helchteren", Country = "België", VenueId = 10 },
-                new Address { Id = 28, Street = "", StreetNumber = 79, PostalCode = "9000", City = "Gent", Country = "België", TicketProviderId = 2 },
-                new Address { Id = 29, Street = "", StreetNumber = 79, PostalCode = "9000", City = "Gent", Country = "België", TicketProviderId = 3 },
-                new Address { Id = 30, Street = "", StreetNumber = 79, PostalCode = "9000", City = "Gent", Country = "België", TicketProviderId = 4 }
+                new Address { Id = 28, Street = "Korte Vlierstraat", StreetNumber = 6, PostalCode = "6200", City = "Antwerpen", Country = "België", TicketProviderId = 2 },
+                new Address { Id = 29, Street = "Blarenberglaan", StreetNumber = 3, PostalCode = "2800", City = "Mechelen", Country = "België", TicketProviderId = 3 },
+                new Address { Id = 30, Street = "Koorstraat", StreetNumber = 17, PostalCode = "3510", City = "Hasselt", Country = "België", TicketProviderId = 5 },
+                new Address { Id = 31, Street = "Braziliestraat", StreetNumber = 26, PostalCode = "2000", City = "Antwerpen", Country = "België", TicketProviderId = 6 }
 
                 );
             });
@@ -142,7 +143,15 @@ namespace YES.Server.Data.Database
             modelBuilder.Entity<TicketProvider>(x =>
             {
                 x.HasData(
-                new TicketProvider { Id = 1, NameProvider = "Vooruit", PhoneNumber = "09 267 28 20", Email = "info@vooruit.be", BankAccount = "BE78 3590 0754 7674" }
+                new TicketProvider { Id = 1, NameProvider = "Vooruit", PhoneNumber = "09 267 28 20", Email = "info@vooruit.be", BankAccount = "BE78 3590 0754 7674" },
+                new TicketProvider { Id = 2, NameProvider = "WAREONE.world bvba", PhoneNumber = "09 147 27 78", Email = "info@tomorrowland.be", BankAccount = "BE78 7854 3585 7820" },
+                new TicketProvider { Id = 3, NameProvider = "Live Nation Festivals NV", PhoneNumber = "09 754 87 78", Email = "info@rockwerchter.be", BankAccount = "BE78 7768 3578 1220" },
+                new TicketProvider { Id = 4, NameProvider = "Couleur Cafe", PhoneNumber = "09 785 24 86", Email = "info@couleurcafe.be", BankAccount = "BE76 5455 8725 7824" },
+                new TicketProvider { Id = 5, NameProvider = "Chokri Mahassine", PhoneNumber = "09 765 78 86", Email = "info@pukkelpop.be", BankAccount = "BE34 8792 4687 2565" },
+                new TicketProvider { Id = 6, NameProvider = "Extrema", PhoneNumber = "09 485 35 41", Email = "info@extremaoutdoor.be", BankAccount = "BE55 7865 7874 1237" },
+                new TicketProvider { Id = 7, NameProvider = "Sportpaleis Group NV", PhoneNumber = "09 879 87 74", Email = "info@sportpaleisgroup.be", BankAccount = "BE55 7865 7874 1237" },
+                new TicketProvider { Id = 8, NameProvider = "eLiXir", PhoneNumber = "09 782 71 42", Email = "info@elixir.be", BankAccount = "BE55 7865 7874 1237" },
+                new TicketProvider { Id = 9, NameProvider = "Team Trix", PhoneNumber = "09 456 79 17", Email = "info@trix.be", BankAccount = "BE55 7865 7874 1237" }
                 );
             });
         }
@@ -156,7 +165,18 @@ namespace YES.Server.Data.Database
                 new TicketCustomer { Id = 2, FirstName = "Ward", LastName = "Impe", PhoneNumber = "0473 422 458", Email = "ward@mail.com", BankAccount = "BE68 6990 5800 7574" },
                 new TicketCustomer { Id = 3, FirstName = "Pieter", LastName = "Corp", PhoneNumber = "0453 288 888", Email = "Pieter@mail.com", BankAccount = "BE60 5590 0994 7021" },
                 new TicketCustomer { Id = 4, FirstName = "Seba", LastName = "Stiaan", PhoneNumber = "0485 345 349", Email = "Seba@mail.com", BankAccount = "BE70 5560 1278 7078" },
-                new TicketCustomer { Id = 5, FirstName = "Nick", LastName = "Angularlover", PhoneNumber = "0478 365 852", Email = "Nick@mail.com", BankAccount = "BE77 7893 0824 7304" }
+                new TicketCustomer { Id = 5, FirstName = "Nick", LastName = "Angularlover", PhoneNumber = "0478 365 852", Email = "Nick@mail.com", BankAccount = "BE77 7893 0824 7304" },
+                new TicketCustomer { Id = 6, FirstName = "Dries", LastName = "Maes", PhoneNumber = "0432 457 896", Email = "Dries@mail.com", BankAccount = "BE41 7563 7835 0157" },
+                new TicketCustomer { Id = 7, FirstName = "Olivia", LastName = "Goossens", PhoneNumber = "0478 365 852", Email = "Olivia@mail.com", BankAccount = "BE96 4278 6420 5496" },
+                new TicketCustomer { Id = 8, FirstName = "Mila", LastName = "Vandevoorde", PhoneNumber = "0485 377 352", Email = "Mila@mail.com", BankAccount = "BE77 1046 8642 5676" },
+                new TicketCustomer { Id = 9, FirstName = "Alice", LastName = "Mcgregor", PhoneNumber = "0478 785 125", Email = "Alice@mail.com", BankAccount = "BE86 7831 5701 5684" },
+                new TicketCustomer { Id = 10, FirstName = "Louise", LastName = "Degroote", PhoneNumber = "0477 765 782", Email = "Louise@mail.com", BankAccount = "BE68 4578 3025 7304" },
+                new TicketCustomer { Id = 11, FirstName = "Mohamed", LastName = "Yilmaz", PhoneNumber = "0472 752 785", Email = "Mohamed@mail.com", BankAccount = "BE89 4785 2015 3065" },
+                new TicketCustomer { Id = 12, FirstName = "Emir", LastName = "Öztürk", PhoneNumber = "0473 478 795", Email = "Emir@mail.com", BankAccount = "BE58 7520 4778 8214" },
+                new TicketCustomer { Id = 13, FirstName = "Kurt", LastName = "Debolle", PhoneNumber = "0478 140 349", Email = "Kurt@mail.com", BankAccount = "BE72 0145 7857 6375" },
+                new TicketCustomer { Id = 14, FirstName = "Arthur", LastName = "Vangeest", PhoneNumber = "0490 785 457", Email = "Arthur@mail.com", BankAccount = "BE86 4576 0445 4873" },
+                new TicketCustomer { Id = 15, FirstName = "Noah", LastName = "Vanarke", PhoneNumber = "0475 850 852", Email = "Noah@mail.com", BankAccount = "BE69 2467 0468 0478" },
+                new TicketCustomer { Id = 16, FirstName = "Victor", LastName = "De Putte", PhoneNumber = "0488 754 752", Email = "Victor@mail.com", BankAccount = "BE88 4785 9785 9620" }
                 );
             });
         }
@@ -175,7 +195,7 @@ namespace YES.Server.Data.Database
                 new Venue { Id = 7, Name = "Rock Werchter", Capacity = 100000 },
                 new Venue { Id = 8, Name = "Couleur Café", Capacity = 60000 },
                 new Venue { Id = 9, Name = "Pukkelpop", Capacity = 100000 },
-                new Venue { Id = 10, Name = "Extrema Outdoor Belgium", Capacity = 60000 }
+                new Venue { Id = 10, Name = "Extrema Outdoor", Capacity = 60000 }
                 );
             });
         }
