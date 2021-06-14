520bd57403c084ccf9d01e52608268be415cbc99
PieterCorp <pieter@email.com>
2021-06-14 13:50:32 +0200

    TicketCustomerWithTradeItems

---
+++ YES/Server/Data/Repos/Interfaces/ITicketCustomerRepo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using YES.Server.Data.Entities;
+
+namespace YES.Server.Data.Repos
+{
+    public interface ITicketCustomerRepo
+    {
+        Task<TicketCustomer> GetEntityAsync(int id);
+    }
+}
---
+++ YES/Shared/Dto/CustomerWithTicketsDto.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace YES.Shared.Dto
+{
+    public class CustomerWithTicketsDto
+    {       
+        public string FirstName { get; set; }       
+        public string LastName { get; set; }
+      
+        public string BankAccount { get; set; }
+        
+        public string Email { get; set; }    
+        
+        public string PhoneNumber { get; set; }   
+
+        public virtual AddressDto Address { get; set; }
+        public virtual ICollection<TicketDto> Tickets { get; set; }
+
+    }
+}
