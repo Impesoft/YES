56ebce2986e6c07f4b84fbdea2cf59eb0eab1d62
PieterCorp <pieter@email.com>
2021-06-14 13:50:32 +0200

    TicketCustomerWithTradeItems

---
+++ YES/Shared/Dto/TicketDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace YES.Shared.Dto
+{
+    public class TicketDto
+    {
+        public int Id { get; set; }
+        public string CustomerFirstName { get; set; }       
+        public string CustomerLastName { get; set; }
+        public string EventName { get; set; }
+        public string VanueName { get; set; }
+        public AddressDto VenueAddress { get; set; }
+        public DateTime DateOfPurchase { get; set; }
+        public int Price { get; set; }
+
+    }
+}
