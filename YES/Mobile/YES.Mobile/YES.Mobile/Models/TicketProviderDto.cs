346c3c5de3688407c2a434b1f46aaf284cb55392
Impesoft <mrblubke@yahoo.com>
2021-06-15 16:46:34 +0200

    renewed Xamarin Project (removed old one restructured

---
+++ YES/Mobile/YES.Mobile/YES.Mobile/Models/TicketProviderDto.cs
@@ -0,0 +1,11 @@
+﻿namespace YES.Mobile.Models
+{
+    public class TicketProviderDto
+    {
+        public int Id { get; set; }
+        public string NameProvider { get; set; }
+        public string BankAccount { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
