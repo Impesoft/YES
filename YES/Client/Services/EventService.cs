cb09fe94f05056508ecb73db54518faf5fca6f97
Nick-vr <nvanroyen@gmail.com>
2021-06-15 16:01:44 +0200

    Make everything light :)

--- YES/Client/Services/EventService.cs
+++ YES/Client/Services/EventService.cs
@@ -5,7 +5,6 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using YES.Shared.Dto;
-using static System.Net.WebRequestMethods;
 
 
 namespace YES.Client.Services
@@ -18,28 +17,31 @@ namespace YES.Client.Services
             _http = http;
         }
 
-
         public async Task<IEnumerable<EventDto>> GetEventsAsync()
         {
-            var _events = await _http.GetFromJsonAsync<ICollection<EventDto>>("https://localhost:44316/api/Event");
+            var _events = await _http.GetFromJsonAsync<ICollection<EventDto>>("https://localhost:5003/api/Event");
 
             return _events.OrderBy(x => x.EventInfo.EventDate);
         }
 
         public async Task<EventDto> GetEventByIdAsync(int id)
         {
-            var _event = await _http.GetFromJsonAsync<EventDto>("https://localhost:44316/api/Event/" + id);
+            var _event = await _http.GetFromJsonAsync<EventDto>("https://localhost:5003/api/Event/" + id);
 
             return _event;
         }
-
-
-
+        
         public async Task<IEnumerable<EventDto>> GetEventSpotlightsAsync()
         {
-            var _events = await _http.GetFromJsonAsync<ICollection<EventDto>>("https://localhost:44316/api/Event");
-            return _events.Take(3);
+            var _events = await _http.GetFromJsonAsync<ICollection<EventDto>>("https://localhost:5003/api/Event");
+            return _events.Take(6);
         }
-
+        
+        //TODO
+        public async Task<IEnumerable<EventDto>> GetFilteredEvents(string userInput)
+        {
+            var _events = await _http.GetFromJsonAsync<ICollection<EventDto>>("https://localhost:5003/api/Event");
+            return _events.Contains(userInput);
+        }
     }
 }
