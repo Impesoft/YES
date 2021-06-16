5c3482d9479bd7b147405dca129c3426998ce0d9
Impesoft <mrblubke@yahoo.com>
2021-06-16 15:14:37 +0200

    to do viewmodels

---
+++ YES/Mobile/YES.Mobile/YES.Mobile/Services/EventService.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using YES.Mobile.Dto;
+
+namespace YES.Mobile.Services
+{
+    public class EventService : IEventService
+    {
+        public async Task<EventDto[]> GetAllEvents()
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
+            HttpClientHandler insecureHandler = handler;
+            HttpClient httpClient = new HttpClient(insecureHandler);
+
+            var resultJson = await httpClient.GetStringAsync("https://yesapi.azurewebsites.net/api/Event");
+
+            var resultEvents = JsonConvert.DeserializeObject<EventDto[]>(resultJson);
+            return resultEvents;
+        }
+    }
+}
