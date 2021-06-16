967b5debf0e09f6b11f062d2bbd052a7d6da895b
Impesoft <mrblubke@yahoo.com>
2021-06-16 18:26:33 +0200

    what the hell is a viewmodel? :p

--- YES/Mobile/YES.Mobile/YES.Mobile/ViewModels/EventsViewModel.cs
+++ YES/Mobile/YES.Mobile/YES.Mobile/ViewModels/EventsViewModel.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using YES.Mobile.Dto;
+using YES.Mobile.Services;
 
 namespace YES.Mobile.ViewModels
 {
     class EventsViewModel : BaseViewModel
     {
+        private static IEventService _eventService { get; set; }
 
-        public EventsViewModel()
+        public EventsViewModel(IEventService eventService)
         {
+            _eventService = eventService;
 
         }
+        public static async void LoadEvents(System.Object sender, System.EventArgs e)
+        {
+            EventDto[] resultEvents = await _eventService.GetAllEvents();
+
+        }
+
     }
 }
