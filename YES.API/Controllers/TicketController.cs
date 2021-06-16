7f8604ac8c017d90e7f8949b7cf11ca9522c55e3
PieterCorp <pieter@email.com>
2021-06-16 14:55:45 +0200

    BuyTickets Endpoint

---
+++ YES.API/Controllers/TicketController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YES.Api.Business.Services;
+using YES.Shared.Dto;
+
+namespace YES.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TicketController : ControllerBase
+    {
+        private readonly ITicketService _ticketService;
+
+        public TicketController(ITicketService ticketService)
+        {
+            _ticketService = ticketService;
+        }
+
+        [HttpPost("/Buy")]
+        public async Task<ActionResult<bool>> BuyTickets(ICollection<TicketPurchaseDto> ticketPurchaseDtos)
+        {
+            return Ok(await _ticketService.BuyTickets(ticketPurchaseDtos));
+        }
+    }
+}
