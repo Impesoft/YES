3baa0759346fdcdf4aca00746f3846044f508a26
SebaK <seba.kesteloot@gmail.com>
2021-06-17 15:24:08 +0200

    update customer form

--- YES/Client/Services/ICustomerService.cs
+++ YES/Client/Services/ICustomerService.cs
@@ -6,6 +6,7 @@ namespace YES.Client.Services
     public interface ICustomerService
     {
         Task<CustomerWithTicketsDto> GetCustomerByIdAsync(int id);
+        Task UpdateCustomer(CustomerWithTicketsDto customer);
         int GetLoggedInUser();
     }
 }
