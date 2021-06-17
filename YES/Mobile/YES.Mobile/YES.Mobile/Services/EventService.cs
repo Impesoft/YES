0964cb65671bb7756b8b752b9ead7e5b8494a92f
Impesoft <mrblubke@yahoo.com>
2021-06-17 16:44:57 +0200

    yet another viewmodel

--- YES/Mobile/YES.Mobile/YES.Mobile/Services/EventService.cs
+++ YES/Mobile/YES.Mobile/YES.Mobile/Services/EventService.cs
@@ -11,7 +11,7 @@ namespace YES.Mobile.Services
 {
     public class EventService : IEventService
     {
-        public async Task<ObservableCollection<EventDto>> GetAllEvents()
+        public ObservableCollection<EventDto> GetAllEvents()
         {
             HttpClientHandler handler = new HttpClientHandler();
             handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
@@ -23,5 +23,19 @@ namespace YES.Mobile.Services
             var resultEvents = JsonConvert.DeserializeObject<ObservableCollection<EventDto>>(resultJson);
             return resultEvents;
         }
+        public EventDto GetEventDetails(int id)
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
+            HttpClientHandler insecureHandler = handler;
+            HttpClient httpClient = new HttpClient(insecureHandler);
+
+            var resultJson = httpClient.GetStringAsync("https://yesapi.azurewebsites.net/api/Event/" + id).Result;
+
+            var eventsDetails = JsonConvert.DeserializeObject<EventDto>(resultJson);
+            return eventsDetails;
+        }
+
+
     }
 }
---
+++ YES/Mobile/YES.Mobile/YES.Mobile/ViewModels/EventDetailsViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using YES.Mobile.Dto;
+using YES.Mobile.Services;
+
+namespace YES.Mobile.ViewModels
+{
+    class EventDetailsViewModel : BaseViewModel
+    {
+        private static EventDto _event;
+        private static IEventService _eventService { get; set; }
+
+        public EventDto Event
+    {
+        get => _event;
+        set
+        {
+            _event = value;
+                OnPropertyChanged(nameof(Event));
+            }
+        }
+    public EventDetailsViewModel()
+    {
+            _eventService = new EventService();
+            Event = new EventDto();
+            LoadEvent(_eventService, id);
+    }
+
+        private void LoadEvent(IEventService eventService, int id)
+        {
+            Event = eventService.GetEventDetails(id);
+                }
+    }
+}
