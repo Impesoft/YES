f79c49d696fa99bfc8c95124ca52789346757b7f
PieterCorp <pieter@email.com>
2021-06-17 15:44:37 +0200

    calc available amount

--- YES.API/Business/Services/TicketService.cs
+++ YES.API/Business/Services/TicketService.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using YES.Api.Data.Entities;
-using YES.Api.Data.Repos.Interfaces;
+using YES.Api.Data.Repos;
 using YES.Shared.Dto;
 
 namespace YES.Api.Business.Services
 {
     public class TicketService :  ITicketService
     {
-        private IGenericRepo<Ticket> _ticketRepo;
-        public TicketService(IGenericRepo<Ticket> ticketRepo)
+        private ITicketRepo _ticketRepo;
+        public TicketService(ITicketRepo ticketRepo)
         {
             _ticketRepo = ticketRepo;
         }
@@ -40,6 +40,21 @@ namespace YES.Api.Business.Services
                 TicketCategoryId = ticketPurchaseDto.TicketCategory.Id,
                 DateOfPurchase = DateTime.Now
             };
+        }
+
+        public async Task<int> GetAmountOfSoldTickets(int eventId, int TicketCategoryId)
+        {
+            int amount = 0;
+            IEnumerable<Ticket> tickets = await _ticketRepo.GetTicketsForEvent(eventId);
+
+            foreach (var ticket in tickets)
+            {
+                if (ticket.TicketCategory.Id == TicketCategoryId)
+                {
+                    amount++;
+                }
+            }
+            return amount;
         }
     }
 }
