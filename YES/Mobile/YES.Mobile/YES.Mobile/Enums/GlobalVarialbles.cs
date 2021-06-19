7feff743fd5bf7dc55078460f5e8dbd94148335e
Impesoft <mrblubke@yahoo.com>
2021-06-19 19:04:08 +0200

    global vars

---
+++ YES/Mobile/YES.Mobile/YES.Mobile/Enums/GlobalVarialbles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YES.Api.Enums
+{
+    public static class GlobalVarialbles
+    {
+        public static string Path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+
+    }
+}
