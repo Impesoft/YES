79f6f841a6459021b71fa45277f13f9c16d00874
Impesoft <mrblubke@yahoo.com>
2021-06-20 01:35:14 +0200

    tis kapoet...

---
+++ YES/Mobile/YES.Mobile/YES.Mobile/Services/IAccountService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using YES.Mobile.Dto;
+
+namespace YES.Mobile.Services
+{
+    public interface IAccountService
+    {
+        string LoggedInUserJson { get; set; }
+        Task<CustomerWithTicketsDto> GetCustomerByIdAsync(int id);
+        UserTokenDto GetLoggedInUser();
+        Task LogIn(LoginDto logindto);
+    }
+}
