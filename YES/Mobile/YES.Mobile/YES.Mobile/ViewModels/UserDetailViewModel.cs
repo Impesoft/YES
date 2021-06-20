80a41d2bdd037ec89d3afd0a7bbfe79540096324
Impesoft <mrblubke@yahoo.com>
2021-06-20 08:58:32 +0200

    added display loggedin user on userdetailpage

--- YES/Mobile/YES.Mobile/YES.Mobile/ViewModels/UserDetailViewModel.cs
+++ YES/Mobile/YES.Mobile/YES.Mobile/ViewModels/UserDetailViewModel.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using YES.Mobile.Enums;
 
 namespace YES.Mobile.ViewModels
 {
     public class UserDetailViewModel : BaseViewModel
     {
+        public string LocalUser { get; set; }
+
         public UserDetailViewModel()
         {
             Title = "User Details";
+            LocalUser = GlobalVariables.LoggedInUser.Email;
         }
     }
 }
