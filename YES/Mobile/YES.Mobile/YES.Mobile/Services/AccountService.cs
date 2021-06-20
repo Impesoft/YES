4c22d4f49bfe81e6aa12fc7015cbf4037d915aea
Impesoft <mrblubke@yahoo.com>
2021-06-20 07:23:41 +0200

    sorry.... iFixedIt

--- YES/Mobile/YES.Mobile/YES.Mobile/Services/AccountService.cs
+++ YES/Mobile/YES.Mobile/YES.Mobile/Services/AccountService.cs
@@ -6,33 +6,40 @@ using System.Web;
 using System.Net;
 using YES.Mobile.Dto;
 using System.Text;
+using Xamarin.Essentials;
+
 
 namespace YES.Mobile.Services
 {
     public class AccountService : IAccountService
     {
         private UserTokenDto LoggedInUser { get; set; }
+        private HttpResponseMessage message;
         public string LoggedInUserJson { get; set; }
         private HttpClient _http;
         public AccountService()
         {
-            _http = new HttpClient();
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
+            HttpClientHandler insecureHandler = handler;
+            _http = new HttpClient(insecureHandler);
         }
 
         public async Task LogIn(LoginDto logindto)
         {
+
             string logindtoJson = JsonConvert.SerializeObject(logindto);
-            StringContent myStringContent = new StringContent(logindtoJson.ToString(), Encoding.UTF8, "application/x-www-form-urlencoded");
-
-            var _loggedInUser = await _http.PostAsync("/api/Account/Login", myStringContent);
-
+            StringContent myStringContent = new StringContent(logindtoJson.ToString(), Encoding.UTF8, "application/json");
+            
+            //message = await _http.PostAsync("https://yesapi.azurewebsites.net/api/Account/Login", myStringContent);
+            int pieter = 0; pieter++;
             //HttpResponseHeader.SetCookie(new (_loggedInUser);
-            HttpResponseMessage message = _http.PostAsync("/api/Account/Login", myStringContent).GetAwaiter().GetResult();
+            message = _http.PostAsync("https://yesapi.azurewebsites.net/api/Account/Login", myStringContent).GetAwaiter().GetResult();
             string responseContent = message.Content.ReadAsStringAsync().GetAwaiter().GetResult();
 
-            LoggedInUserJson = await _loggedInUser.Content.ReadAsStringAsync();
+            LoggedInUserJson = await message.Content.ReadAsStringAsync();
 
-            if (_loggedInUser != null)
+            if (LoggedInUserJson != null)
             {              
                 LoggedInUser = JsonConvert.DeserializeObject<UserTokenDto>(LoggedInUserJson);
 
@@ -40,12 +47,14 @@ namespace YES.Mobile.Services
             }           
         }
 
-        //public async Task<CustomerWithTicketsDto> GetCustomerByIdAsync(int id)
-        //{
-        //    var _customer = await _http.GetFromJsonAsync<CustomerWithTicketsDto>("api/TicketCustomer/IncludeTickets/" + id);
+        public async Task<CustomerWithTicketsDto> GetCustomerByIdAsync(int id)
+        {
 
-        //    return _customer;
-        //}
+            //   var _customer = await _http.GetFromJsonAsync<CustomerWithTicketsDto>("api/TicketCustomer/IncludeTickets/" + id);
+            var content = await _http.GetStringAsync("api/TicketCustomer/IncludeTickets/" + id);
+            return JsonConvert.DeserializeObject<CustomerWithTicketsDto>(content);
+
+        }
 
 
         public UserTokenDto GetLoggedInUser()
