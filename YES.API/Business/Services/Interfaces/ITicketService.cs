4d947a50e341a5467fc9b7ca8145a33cb6028783
PieterCorp <pieter@email.com>
2021-06-20 17:25:53 +0200

    CancelTickets

--- YES.API/Business/Services/Interfaces/ITicketService.cs
+++ YES.API/Business/Services/Interfaces/ITicketService.cs
@@ -7,6 +7,7 @@ namespace YES.Api.Business.Services
     public interface ITicketService
     {
         Task<bool> BuyTickets(IEnumerable<TicketPurchaseDto> ticketPurchaseDtos);
+        Task<bool> CancelTickets(IEnumerable<int> canceledTicketIds);
         int GetAmountOfSoldTickets(int eventId, int TicketCategoryId);
     }
 }
