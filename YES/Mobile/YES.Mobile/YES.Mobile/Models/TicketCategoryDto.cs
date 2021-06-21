d1e943a133beb165d178bf6032cd6538918c9258
Kobe Delobelle <delobellekobe@gmail.com>
2021-06-21 14:59:01 +0200

    EventDetailViewModel prutswerk

--- YES/Mobile/YES.Mobile/YES.Mobile/Models/TicketCategoryDto.cs
+++ YES/Mobile/YES.Mobile/YES.Mobile/Models/TicketCategoryDto.cs
@@ -6,5 +6,6 @@
         public string Name { get; set; }
         public double Price { get; set; }
         public int MaxAmount { get; set; }
+        public int AvailableAmount { get; set; }
     }
 }
