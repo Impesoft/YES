15b7e30a1ee7c931a88bb83b8ed01ef55f850cc4
Nick-vr <nvanroyen@gmail.com>
2021-06-22 16:22:15 +0200

    Login Modal

--- YES/Shared/Dto/UserTokenDto.cs
+++ YES/Shared/Dto/UserTokenDto.cs
@@ -6,6 +6,10 @@ namespace YES.Shared.Dto
     {
         public int Id { get; set; }
         public string Email { get; set; }
+        
+        public string  FirstName { get; set; }
+        
+        public string  NameProvider { get; set; }
         public string Token { get; set; }
         public Roles Role { get; set; }
     }
