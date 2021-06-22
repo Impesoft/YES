1070948ad6f6872d69dcd2a3321bc5ec4e74af05
Impesoft <mrblubke@yahoo.com>
2021-06-22 10:01:41 +0200

    expiry date display on userdetails

--- YES/Mobile/YES.Mobile/YES.Mobile/ViewModels/UserDetailViewModel.cs
+++ YES/Mobile/YES.Mobile/YES.Mobile/ViewModels/UserDetailViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Text;
+using YES.Mobile.Dto;
 using YES.Mobile.Enums;
 
 namespace YES.Mobile.ViewModels
@@ -8,11 +10,28 @@ namespace YES.Mobile.ViewModels
     public class UserDetailViewModel : BaseViewModel
     {
         public string LocalUser { get; set; }
+        private DateTime? expiryDate;
+
+        public DateTime? ExpiryDate
+        {
+            get => expiryDate;
+            set
+            {
+                expiryDate = value;
+                OnPropertyChanged(nameof(ExpiryDate));
+            }
+        }
 
         public UserDetailViewModel()
         {
             Title = "User Details";
             LocalUser = GlobalVariables.LoggedInUser.Email;
+            UserTokenDto user = GlobalVariables.LoggedInUser;
+            var stream = user.Token;
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = handler.ReadToken(stream);
+            var tokenS = jsonToken as JwtSecurityToken;
+            expiryDate = tokenS.ValidTo;
         }
     }
 }
