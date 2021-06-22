3147bee4d49c737e440a362aa51be51d0f338c4a
Impesoft <mrblubke@yahoo.com>
2021-06-22 14:09:20 +0200

    added to dto in shared FirstName & NameProvider

--- YES/Shared/Dto/UserTokenDto.cs
+++ YES/Shared/Dto/UserTokenDto.cs
@@ -6,6 +6,8 @@ namespace YES.Shared.Dto
     {
         public int Id { get; set; }
         public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string NameProvider { get; set; }
         public string Token { get; set; }
         public Roles Role { get; set; }
     }
