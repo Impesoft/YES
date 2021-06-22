bf60e132670501e93e11dc87a298ef3481583257
Impesoft <mrblubke@yahoo.com>
2021-06-22 23:15:05 +0200

    xamarin more work

--- YES/Mobile/YES.Mobile/YES.Mobile/ViewModels/UserDetailViewModel.cs
+++ YES/Mobile/YES.Mobile/YES.Mobile/ViewModels/UserDetailViewModel.cs
@@ -4,13 +4,28 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 using YES.Mobile.Dto;
 using YES.Mobile.Enums;
+using YES.Mobile.Services;
 
 namespace YES.Mobile.ViewModels
 {
     public class UserDetailViewModel : BaseViewModel
     {
-        public string LocalUser { get; set; }
+        //  public string LocalUser { get; set; }
         private DateTime? expiryDate;
+
+        private CustomerWithTicketsDto _localUser;
+
+        public CustomerWithTicketsDto LocalUser
+        {
+            get => _localUser;
+            set
+            {
+                _localUser = value;
+                OnPropertyChanged(nameof(LocalUser));
+            }
+        }
+
+        public ICustomerService CustomerService { get; set; }
 
         public DateTime? ExpiryDate
         {
@@ -25,13 +40,19 @@ namespace YES.Mobile.ViewModels
         public UserDetailViewModel()
         {
             Title = "User Details";
-            LocalUser = GlobalVariables.LoggedInUser.Email;
+            CustomerService = new CustomerService();
+            LoadUserWithTickets();
             UserTokenDto user = GlobalVariables.LoggedInUser;
             var stream = user.Token;
             var handler = new JwtSecurityTokenHandler();
             var jsonToken = handler.ReadToken(stream);
             var tokenS = jsonToken as JwtSecurityToken;
             expiryDate = tokenS.ValidTo;
+        }
+
+        private async void LoadUserWithTickets()
+        {
+            LocalUser = await CustomerService.GetCustomerAsync();
         }
     }
 }
