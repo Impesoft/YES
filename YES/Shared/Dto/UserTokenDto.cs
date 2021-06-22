240f6539ff1fe8faca1f6fbba7ee32901727911f
PieterCorp <pieter@email.com>
2021-06-22 14:54:19 +0200

    add name to UserTokenDto

--- YES/Shared/Dto/UserTokenDto.cs
+++ YES/Shared/Dto/UserTokenDto.cs
@@ -5,6 +5,8 @@ namespace YES.Shared.Dto
     public class UserTokenDto
     {
         public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string NameProvider { get; set; }
         public string Email { get; set; }
         public string Token { get; set; }
         public Roles Role { get; set; }
