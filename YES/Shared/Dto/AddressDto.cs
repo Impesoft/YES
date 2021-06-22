a4dcf199d63607da5d0bf13e52ea93c0a2aff48b
Seba Kesteloot <seba.kesteloot@gmail.com>
2021-06-22 11:24:33 +0200

    Added user form validation + data annotations in relevant DTOs

--- YES/Shared/Dto/AddressDto.cs
+++ YES/Shared/Dto/AddressDto.cs
@@ -1,12 +1,30 @@
-﻿namespace YES.Shared.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YES.Shared.Dto
 {
     public class AddressDto
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter your street")]
+        [MaxLength(30, ErrorMessage = "Maximum length is 30 characters.")]
         public string Street { get; set; }
+
+        [Required(ErrorMessage = "Please enter your postal code.")]
+        [MaxLength(10, ErrorMessage = "Maximum length is 10 characters.")]
+        [MinLength(3, ErrorMessage = "Minimum length is 3 characters.")]
         public string PostalCode { get; set; }
+
+        [Required(ErrorMessage = "Please enter your city")]
+        [MaxLength(30, ErrorMessage = "Maximum length is 30 characters.")]
         public string City { get; set; }
+
+        [Required(ErrorMessage = "Please enter your street number")]
+        [MaxLength(5, ErrorMessage = "Maximum length is 5 characters.")]
         public string StreetNumber { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Country")]
+        [MaxLength(30, ErrorMessage = "Maximum length is 30 characters.")]
         public string Country { get; set; }      
     }
 }
