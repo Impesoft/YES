8fd6ddc71123df216c7b797771546368e3bbb2e8
PieterCorp <pieter@email.com>
2021-06-23 15:43:11 +0200

    NoName

--- YES/Shared/Dto/UserTokenDto.cs
+++ YES/Shared/Dto/UserTokenDto.cs
@@ -4,10 +4,7 @@ namespace YES.Shared.Dto
 {
     public class UserTokenDto
     {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string NameProvider { get; set; }
+        public int Id { get; set; }       
         public string Email { get; set; }       
         public string Token { get; set; }
         public Roles Role { get; set; }
