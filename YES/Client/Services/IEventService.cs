2bfae490c153de164532dc28e9ad84deb150f3ee
SebaK <seba.kesteloot@gmail.com>
2021-06-23 17:32:01 +0200

    Create new Event

--- YES/Client/Services/IEventService.cs
+++ YES/Client/Services/IEventService.cs
@@ -9,5 +9,7 @@ namespace YES.Client.Services
         Task<IEnumerable<EventDto>> GetEventsAsync();
         Task<EventDto> GetEventByIdAsync(int id);
         Task<IEnumerable<EventDto>> GetEventSpotlightsAsync();
+        Task<bool> CreateNewEventAsync(EventDto eventDto);
+        Task<IEnumerable<VenueDto>> GetVenuesAsync();
     }
 }
