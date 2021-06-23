5da3ecf48e3661f0651ad606b347967c50a90ca7
PieterCorp <pieter@email.com>
2021-06-23 03:27:19 +0200

    Invoice by mail

--- YES.API/Business/Services/EmailService.cs
+++ YES.API/Business/Services/EmailService.cs
@@ -1,13 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Net;
+using System.Net.Mail;
 
 namespace YES.Api.Business.Services
 {
-    public class EmailService
+    public class EmailService : IEmailService
     {
+        private bool succes = false;
 
+        public bool SendEmail(string eMailreceiver, string nameReceiver, string subject, string messageHtml)
+        {
+            SmtpClient Client = new()
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential()
+                {
+                    UserName = "yes.youreventservice@gmail.com",
+                    Password = "***REMOVED***"
+                }
+            };
+            MailAddress fromEmail = new("yes.youreventservice@gmail.com", "YesTeam");
+            MailAddress toEmail = new(eMailreceiver, nameReceiver);
+            MailMessage message = new()
+            {
+                From = fromEmail,
+                Subject = subject,
+                Body = messageHtml,
+                IsBodyHtml = true
+            };
+            message.To.Add(toEmail);
+            Client.SendCompleted += Client_SendCompleted;
+            Client.SendMailAsync(message);
 
+            return succes;
+        }
+
+        private void Client_SendCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                succes = false;
+                return;               
+            }
+            succes = true;
+        }
     }
 }
