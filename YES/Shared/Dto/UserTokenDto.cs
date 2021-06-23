70a483d56bb57e3211b060eb40f403ed8a3966e1
SebaK <seba.kesteloot@gmail.com>
2021-06-23 09:10:47 +0200

    fix build errors

--- YES/Shared/Dto/UserTokenDto.cs
+++ YES/Shared/Dto/UserTokenDto.cs
@@ -7,9 +7,7 @@ namespace YES.Shared.Dto
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string NameProvider { get; set; }
-        public string Email { get; set; }
-        public string FirstName { get; set; }
-        public string NameProvider { get; set; }
+        public string Email { get; set; }       
         public string Token { get; set; }
         public Roles Role { get; set; }
     }
