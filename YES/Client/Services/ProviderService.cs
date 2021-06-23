0557854f2ac81eea10f3d02a2757d062dec9039c
SebaK <seba.kesteloot@gmail.com>
2021-06-23 17:32:01 +0200

    Create new Event

---
+++ YES/Client/Services/ProviderService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using YES.Shared.Dto;
+
+namespace YES.Client.Services
+{
+    public class ProviderService : IProviderService
+    {
+        private HttpClient _http;
+
+        public ProviderService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<TicketProviderDto> GetProviderByIdAsync(int id)
+        {
+            var provider = await _http.GetFromJsonAsync<TicketProviderDto>("api/TicketProvider/" + id);
+
+            return provider;
+        }
+
+        public async Task UpdateProvider(TicketProviderDto provider)
+        {
+            var response = await _http.PutAsJsonAsync("api/TicketProvider", provider);
+            Console.Write(response);
+        }
+    }
+}
