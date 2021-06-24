c8097db3fb7c49e7ff9250c281224a13de2ad4d2
PieterCorp <pieter@email.com>
2021-06-24 14:22:35 +0200

    Post and Update event Fix

--- YES/Shared/Dto/UserTokenDto.cs
+++ YES/Shared/Dto/UserTokenDto.cs
@@ -4,7 +4,8 @@ namespace YES.Shared.Dto
 {
     public class UserTokenDto
     {
-        public int Id { get; set; }       
+        public int Id { get; set; }
+        public string GreetingName { get; set; }
         public string Email { get; set; }       
         public string Token { get; set; }
         public Roles Role { get; set; }
