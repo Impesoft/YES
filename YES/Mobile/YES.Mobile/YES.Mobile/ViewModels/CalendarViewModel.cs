548250603e8d4745d6e48d6e112a3ea7284e870a
Impesoft <mrblubke@yahoo.com>
2021-06-25 12:42:15 +0200

    searchbar inprogres interupted part....VI

--- YES/Mobile/YES.Mobile/YES.Mobile/ViewModels/CalendarViewModel.cs
+++ YES/Mobile/YES.Mobile/YES.Mobile/ViewModels/CalendarViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -16,14 +17,17 @@ namespace YES.Mobile.ViewModels
     public class CalendarViewModel : BaseViewModel
     {
         private ICollection<EventDto> _events;
+        private ICollection<EventDto> _eventsFiltered;
         private IEventService _eventService { get; set; }
 
         //public Command<EventDto> EventTappedCommand => new Command<EventDto>(OnEventSelected);
         public Command<string> FilterListCommand { get; }
+
         public Command<EventDto> EventTappedCommand { get; }
 
         public ICommand EventLoadCommand { get; set; }
         public bool DBIsBusy { get; set; }
+        public string SearchTerm { get; set; }
 
         public ICollection<EventDto> Events
         {
@@ -32,6 +36,17 @@ namespace YES.Mobile.ViewModels
             {
                 _events = value;
                 OnPropertyChanged(nameof(Events));
+                _eventsFiltered = Events.Where(x => x.EventInfo.Description.ToLowerInvariant().Contains(SearchTerm)).ToList();
+            }
+        }
+
+        public ICollection<EventDto> EventsFiltered
+        {
+            get => _eventsFiltered;
+            set
+            {
+                _eventsFiltered = value;
+                OnPropertyChanged(nameof(EventsFiltered));
             }
         }
 
@@ -41,6 +56,7 @@ namespace YES.Mobile.ViewModels
 
             _eventService = new EventService();
             Events = new ObservableCollection<EventDto>();
+            EventsFiltered = new ObservableCollection<EventDto>();
             Task.Run(() => LoadEvents());
 
             EventLoadCommand = new Command((async () => await LoadEvents()));
