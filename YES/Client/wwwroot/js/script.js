53bf6ced34c5c9f9bb8cc14b8f746004b497fccb
PieterCorp <pieter@email.com>
2021-06-29 16:28:27 +0200

    mapbox is free

--- YES/Client/wwwroot/js/script.js
+++ YES/Client/wwwroot/js/script.js
@@ -1,27 +1,37 @@
 ﻿
-//function initialize(address) {
-
-//    console.log(address.city);
-//    console.log(address.street);
-//    var latlng = new google.maps.LatLng(40.716948, -74.003563);
-//    var options = {
-//        zoom: 14, center: latlng,
-//        mapTypeId: google.maps.MapTypeId.ROADMAP
-//    };
-//    var map = new google.maps.Map(document.getElementById("map"), options);
-//}
-
 function initialize(address) {
 
+    console.log(address.city);
+    console.log(address.street);
+
     mapboxgl.accessToken = 'pk.eyJ1IjoieW91cmV2ZW50c2VydmljZSIsImEiOiJja3FodWViZ2cwZWprMm5xanZidGpxcDczIn0.pWZhZF15SwNTSfAcKZHjWw';
-    var map = new mapboxgl.Map({
-        container: 'map', // container ID
-        style: 'mapbox://styles/youreventservice/ckqhunikm1i4m17pfij2t3ubi', // style URL
-        center: [-74.5, 40], // starting position [lng, lat]
-        zoom: 9 // starting zoom
-    });
+    var mapboxClient = mapboxSdk({ accessToken: mapboxgl.accessToken });
+    mapboxClient.geocoding
+        .forwardGeocode({
+            query: `${address.street}, ${address.city}`,
+            autocomplete: false,
+            limit: 1
+        })
+        .send()
+        .then(function (response) {
+            if (
+                response &&
+                response.body &&
+                response.body.features &&
+                response.body.features.length
+            ) {
+                var feature = response.body.features[0];
 
+                var map = new mapboxgl.Map({
+                    container: 'map',
+                    style: 'mapbox://styles/youreventservice/ckqi4infi0ahu17n3q9u8razd',
+                    center: feature.center,
+                    zoom: 10
+                });
+
+                // Create a marker and add it to the map.
+                new mapboxgl.Marker().setLngLat(feature.center).addTo(map);
+            }
+        });
+   
 }
-
-
-
