377e490ca483e10493ec767778c3bbaa4e78f224
PieterCorp <pieter@email.com>
2021-06-29 10:34:25 +0200

    dataAnnotations Entities

--- YES.API/Data/Entities/TicketCustomer.cs
+++ YES.API/Data/Entities/TicketCustomer.cs
@@ -6,9 +6,11 @@ namespace YES.Api.Data.Entities
     public class TicketCustomer : User
     {
         [Required]
+        [MaxLength(50)]
         public string FirstName { get; set; }
 
         [Required]
+        [MaxLength(50)]
         public string LastName { get; set; }
 
         public virtual ICollection<Ticket> Tickets { get; set; }
