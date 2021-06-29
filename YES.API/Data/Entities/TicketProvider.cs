45dadf4f591bf4df9f437510388c7801a871fba4
PieterCorp <pieter@email.com>
2021-06-29 10:34:25 +0200

    dataAnnotations Entities

--- YES.API/Data/Entities/TicketProvider.cs
+++ YES.API/Data/Entities/TicketProvider.cs
@@ -6,6 +6,7 @@ namespace YES.Api.Data.Entities
     public class TicketProvider : User
     {
         [Required]
+        [MaxLength(50)]
         public string NameProvider { get; set; }
 
         public virtual ICollection<Event> Events { get; set; }
